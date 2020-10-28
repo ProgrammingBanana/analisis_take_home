c93b9717673da7e137ac549608a6e7d7a3bd1d1f
ProgrammingBanana <daniel.jesus@upr.edu>
2020-10-28 16:46:58 -0400

    Added conditions for left movement and logic to mark path in array

--- rabbit_hole.py
+++ rabbit_hole.py
@@ -15,26 +15,42 @@ class RabbitHole():
             print("")
     print(']')
 
-  def compare(self, x, y):
-    if x == "*" and y == "*":
+  def compare_three(self, x, y, z):
+    if x == "*" and y == "*" and z =="*":
         return "*"
-    return y if x == "*" else x
+    elif x != "*":
+        return x
+    elif y != "*":
+        return y
+    else:
+        return z
     
-  def rabbit_path_helper(self, row, col):
-    if row < 0 or col < 0 or self.board[row][col] == 'o':
+  def rabbit_path_helper(self, row, col, origin):
+    if row < 0 or col < 0 or col >= self.col or self.board[row][col] == 'o':
         return "*"
     elif row == 0 and col == 0:
         return "(0,0)"
     else:
-        return self.compare(self.rabbit_path_helper(row-1, col),self.rabbit_path_helper(row, col-1)) + " -> (" +str(row) +"," +str(col) +")"
+        if origin == "right":
+            return self.compare_three(self.rabbit_path_helper(row-1, col, "down"),self.rabbit_path_helper(row, col-1, "right"), "*") + " -> (" +str(row) +"," +str(col) +")"
+        elif origin == "left":
+            return self.compare_three(self.rabbit_path_helper(row-1, col, "down"), "*", self.rabbit_path_helper(row, col+1, "left")) + " -> (" +str(row) +"," +str(col) +")"
+        else:
+            return self.compare_three(self.rabbit_path_helper(row-1, col, "down"), self.rabbit_path_helper(row, col-1, "right"), self.rabbit_path_helper(row, col+1, "left")) + " -> (" +str(row) +"," +str(col) +")"
 
   def rabbit_path(self):
-    path = self.rabbit_path_helper(self.row-1, self.col-1)
-    return path if "*" not in path else "There is no path" 
+    path = self.rabbit_path_helper(self.row-1, self.col-1, "x")
+    if "*" in path:
+        return "There is no Path"
+    stripped_path = path.replace(")"," ").replace("->", "").replace("(","").split("   ")
+    for indices in stripped_path:
+        self.board[int(indices[0])][int(indices[2])] = 1
+    return path
 
 
 
 if __name__ == "__main__" :
+    print("Rabbit 1 (Example of only down and right movement)")
     arr = [['A', 'o', 'o', 'o'],
             ['A', 'A', 'A', 'A'],
             ['o', 'o', 'A', 'o'],
@@ -42,5 +58,29 @@ if __name__ == "__main__" :
             ['o', 'o', 'o', 'A']]
 
     burrow = RabbitHole(arr)
-    burrow.print_burrow()
     print(burrow.rabbit_path())
+    burrow.print_burrow()
+
+    print("\nRabbit 2 (Example of all possible movement, left, right and down)")
+    arr = [['A', 'A', 'o', 'o'],
+            ['o', 'A', 'A', 'A'],
+            ['o', 'o', 'o', 'A'],
+            ['o', 'A', 'A', 'A'],
+            ['o', 'A', 'o', 'o'],
+            ['o', 'A', 'A', 'A']]
+
+    burrow = RabbitHole(arr)
+    print(burrow.rabbit_path())
+    burrow.print_burrow()
+
+    print("\nRabbit 3 (Example of no possible path)")
+    arr = [['A', 'A', 'o', 'o'],
+            ['o', 'A', 'A', 'A'],
+            ['o', 'o', 'o', 'A'],
+            ['o', 'A', 'A', 'A'],
+            ['o', 'o', 'o', 'o'],
+            ['o', 'A', 'A', 'A']]
+
+    burrow = RabbitHole(arr)
+    print(burrow.rabbit_path())
+    burrow.print_burrow()
