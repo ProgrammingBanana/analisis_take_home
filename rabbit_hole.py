9719a099b2ce8e1baf1f0bab95d68e1e71968cef
ProgrammingBanana <daniel.jesus@upr.edu>
2020-10-27 22:34:21 -0400

    Added methods to find the path to the burrow

--- rabbit_hole.py
+++ rabbit_hole.py
@@ -15,19 +15,34 @@ class RabbitHole():
                 print("")
         print(']')
 
+    def compare(self, x, y):
+        if x == "*" and y == "*":
+            return "*"
+        return y if x == "*" else x
+        
+    def rabbit_path_helper(self, row, col):
+        if row < 0 or col < 0 or self.board[row][col] == 'o':
+            return "*"
+        elif row == 0 and col == 0:
+            return "(0,0)"
+        else:
+            return self.compare(self.rabbit_path_helper(row-1, col),self.rabbit_path_helper(row, col-1)) + " -> (" +str(row) +"," +str(col) +")"
 
-    
+    def rabbit_path(self):
+        path = self.rabbit_path_helper(self.row-1, self.col-1)
+        return path if "*" not in path else "There is no path" 
+
     def print_size(self):
         return self.row, self.col
 
 
-arr = [['a', 'o', 'o', 'o'],
-        ['a', 'a', 'a', 'a'],
-        ['o', 'o', 'a', 'o'],
-        ['o', 'a', 'a', 'a'],
-        ['o', 'o', 'o', 'a']]
+if __name__ == "__main__" :
+    arr = [['A', 'o', 'o', 'o'],
+            ['A', 'A', 'A', 'A'],
+            ['o', 'o', 'A', 'o'],
+            ['o', 'A', 'A', 'A'],
+            ['o', 'o', 'o', 'A']]
 
-burrow = RabbitHole(arr)
-
-burrow.print_burrow()
-print(burrow.print_size())
+    burrow = RabbitHole(arr)
+    burrow.print_burrow()
+    print(burrow.rabbit_path())
